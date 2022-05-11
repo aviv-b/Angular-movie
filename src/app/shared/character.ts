d93221f120f94413b7cbb145536f46bc1dd967b3
Aviv Biton <51339282+aviv-b@users.noreply.github.com>
2022-05-11 12:38:21 +0300

    update

--- src/app/shared/character.ts
+++
@@ -1,9 +0,0 @@
-export interface ICharacter{
-    name:string
-    height:Date 
-    mass:string 
-    hair_color:string 
-    skin_color:string
-    eye_color:string
-    birth_year:string 
-}
--- src/app/shared/film.ts
+++
@@ -1,7 +0,0 @@
-export interface IFilm{
-    title:string
-    release_date:Date 
-    opening_crawl:string 
-    producer :string 
-    characters:string[]
-}
